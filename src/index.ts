a5c266908c651a8c67cae7f90548bea5d124ace0
Eric Nilsson <eric.nilsson@hotmail.com>
2020-07-07 15:38:50 +0200

    chore(jwt): specify jwt algorithm

--- src/index.ts
+++ src/index.ts
@@ -90,6 +90,7 @@ async function bootstrap() {
                 secret: process.env.JWT_SECRET,
                 credentialsRequired: false,
                 userProperty: 'tokenData',
+                algorithms: ['RS256'],
                 getToken: (req) => {
                     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
                         return req.headers.authorization.split(' ')[1];
